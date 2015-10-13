899479fddd0c040e78d0db6154c7429f4b2ec944
stoilyanchev <stoilyanchev@abv.bg>
2015-10-13 16:19:46 +0300

    First commit

---
+++ ASPNETWcfTest/WcfTestWebSite/App_Code/PairArihmeticService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+public class PairArihmeticService : IPairArihmeticService
+{
+    Pair IPairArihmeticService.Add(Pair p1, Pair p2)
+    {
+        Pair result = new Pair();
+
+        result.First = p1.First + p2.First;
+        result.Second = p1.Second + p2.Second;
+
+        return result;
+    }
+
+    Pair IPairArihmeticService.Subtract(Pair p1, Pair p2)
+    {
+        Pair result = new Pair();
+
+        result.First = p1.First - p2.First;
+        result.Second = p1.Second - p2.Second;
+
+        return result;
+    }
+}
